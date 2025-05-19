e3e490e826a0d63bd7c4768e0b0d26d78b277e45
liuhf <523005941@qq.com>
2025-05-19 22:18:12 +0800

    12.6 success

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,13 +16,18 @@ class Ship:
 
         # 对于每艘新飞船，都将其放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
+        # 在飞船的属性x中存储小数值
+        self.x = float(self.rect.x)
         # 移动标识（飞船一开始不移动）
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
